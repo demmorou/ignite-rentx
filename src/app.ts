60391e8af0eaeaca357248e02124aff49e8d7a6d
deusimardamiao <sousadeusimar2@gmail.com>
2021-05-14 07:22:44 -0300

    feat(docs): add specifications and import category docs

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
+import cors from 'cors';
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
-
 import './database';
 import './shared/container';
 
@@ -10,6 +10,7 @@ import swagger from './swagger.json';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swagger));
