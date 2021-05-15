58a287b2e2af2e2bca9b0a4d67a07ed7f96cdc36
deusimardamiao <sousadeusimar2@gmail.com>
2021-05-15 15:18:01 -0300

    feat(sessions): added route to handle request on authenticate an user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { authenticateRoutes } from './authenticate.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { userRoutes } from './users.routes';
@@ -9,5 +10,6 @@ const routes = Router();
 routes.use('/categories', categoriesRoutes);
 routes.use('/specifications', specificationsRoutes);
 routes.use('/users', userRoutes);
+routes.use(authenticateRoutes);
 
 export { routes };
